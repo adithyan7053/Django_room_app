38a67ebbca4bfa5e253e199362ff09565b5954f5
adithyan7053 <adithyan7053@gmail.com>
2024-05-31 21:52:56 +0530

    first commit

---
+++ studybud/base/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name = "login"),
+    path('register/', views.registerUser, name = "register"),
+    path('logout/', views.logoutUser, name = "logout"),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+    path('update-message/', views.updateUser, name="update-user"),
+
+]
